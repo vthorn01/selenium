6e150df3d1d3332fa06dba1307d951eb32b1f36a
vthorn01 <vanarothorn@outlook.com>
2022-12-19 22:31:10 -0600

    Update browser_driver_and_path.py
    added chrome driver to system

--- browser_driver_and_path.py
+++ browser_driver_and_path.py
@@ -1,1 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
+
+
+se = Service(executable_path="C:\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=se)
+
+driver.get("http://www.python.org")
+time.sleep(5000)
